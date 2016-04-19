600bac47bf793d86961b3627d50fe4367537cd6b
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2016-04-19 14:54:24 +0530

    add headers

--- tags.php
+++ tags.php
@@ -28,24 +28,16 @@
 
 <body>
 
-<nav class="navbar navbar-fixed-top">
+<header class="navbar navbar-fixed-top">
     <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Project name</a>
+        <div class="pull-left brand float-remove-xs text-center-xs">
+            <a href="index.php">
+                <img src="/images/logo.svg" alt="wso2" title="wso2" class="logo">
+                <h1 class="display-block-xs">WSO2 Docker Repositories</h1>
+            </a>
         </div>
-        <div id="navbar" class="collapse navbar-collapse">
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="index.html">Home</a></li>
-            </ul>
-        </div><!--/.nav-collapse -->
     </div>
-</nav>
+</header>
 
 <div class="container">
 
