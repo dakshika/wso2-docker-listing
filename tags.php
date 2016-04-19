962054ae78ac453c237bd7e21b16a111e8bdaee9
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2016-04-19 14:20:27 +0530

    improve the code

--- tags.php
+++ tags.php
@@ -78,8 +78,9 @@
 <script>
     $(document).ready(function() {
         $('.block-no-bulet').html('');
+        var getTagName = GetURLParameter('repo');
         var text = '';
-        $.getJSON( "tags.json", function( data ) {
+        $.getJSON( "tags.json?repo="+getTagName, function( data ) {
             var repositories = data.tags;
             for (i = 0; i < repositories.length; i++) {
             text += '<tr><td>'+repositories[i]+'</td></tr>';
@@ -89,6 +90,21 @@
 
 
     });
+
+
+    function GetURLParameter(sParam)
+    {
+        var sPageURL = window.location.search.substring(1);
+        var sURLVariables = sPageURL.split('&');
+        for (var i = 0; i < sURLVariables.length; i++)
+        {
+            var sParameterName = sURLVariables[i].split('=');
+            if (sParameterName[0] == sParam)
+            {
+                return sParameterName[1];
+            }
+        }
+    }​
 
 </script>
 </body>
